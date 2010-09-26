307a08edd61efa4ad7494b51566919c1a1fa0be6
Josh Mahowald <jmahowald@angel.com>
2010-09-26 00:39:59 -0400

    changing to now have gitifnore but to make empty directories to create necessary structure

--- phpbase/build.sh
+++ phpbase/build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+mkdir -p buildroot/usr/local/angel.com/psphp
+mkdir -p buildroot/usr/local/angel.com/psphpconfig
+mkdir -p buildroot/var/log/angel
 rpmbuild --bb SPECS/angel_php_base.spec --buildroot `pwd`/buildroot --define="_topdir `pwd`"
