b4eb16d0a56fc954ebb3e55301d7057afa5359b8
prateek <sudheer_pn@spanservices.com>
2010-10-04 15:18:15 +0500

    deleting rpm_test.py. As its not required anymore

--- rpm_test.py
+++
@@ -1,8 +0,0 @@
-from rpm_creator import *
-from rpm_yum_update import *
-rpm = CreateRPMPackage("script","1.0","1.0","server","/usr/local/angel.com","/home/sudheer")
-rpm.createRPM()
-yum = UpdateYumRepository("/home/sudheer/RPMS/noarch","/home/repo")
-
-
-
