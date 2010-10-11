f487956efb72cfeefde97ae18c98fcd30a6e920c
Josh Mahowald <jmahowald@angel.com>
2010-10-11 14:48:05 -0400

    remove yum clean and list which are unecessary and require one to be root

--- rpm_yum_update.py
+++ rpm_yum_update.py
@@ -20,22 +20,11 @@ class UpdateYumRepository:
     def createRepository(self):
         if os.path.exists(self.__yumRepoDir):
             self.copyRPMs()
-            self.cleanYumRepository()
             print "Executing CreateRepo command....................................."
             os.system("createrepo   " +self.__yumRepoDir)
-            self.yumList()
         else:
             print("Yum Local Reository does not exist")    
 
-    def cleanYumRepository(self):
-        print "Executing Yum clean command.........................................."
-        #  Run this command to   ensure that, nothing unnecessary space used.
-        os.system("yum clean all")
-
-    def yumList(self):
-        print "Executing Yum list comannd........................................"
-        os.system("yum list | grep angel")
-        
        
                  
 
